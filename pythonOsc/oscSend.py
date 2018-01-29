c7aa90672c58c84d2563707f7350f2b2193ba5b7
farrrell504 <farrrell504@gmail.com>
2018-01-28 21:04:37 -0500

    Update oscSend.py

--- pythonOsc/oscSend.py
+++ pythonOsc/oscSend.py
@@ -9,7 +9,8 @@ Created on Fri Nov  3 22:15:07 2017
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 client = udp_client.UDPClient('192.168.1.18', 7562)
-msg = osc_message_builder.OscMessageBuilder(address = '/osc-test')
+#msg = osc_message_builder.OscMessageBuilder(address = '/osc-test')
+msg = osc_message_builder.OscMessageBuilder(address = '/whoisthis')
 msg.add_arg('150')
 #msg.add_arg('3.0')
 msg = msg.build()
